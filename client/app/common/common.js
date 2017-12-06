d0363260540408526c6ac8ed2c8543cb09d67b82
Roland Oben <rooben@rogers.com>
2017-12-06 02:43:50 -0500

    Did some further re-arrangements, deleted some unnecessary folders.

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -2,13 +2,11 @@ import angular from 'angular';
 import Navbar from './navbar/navbar';
 import Hero from './hero/hero';
 import TeamService from './services/first-service';
-import User from './user/user';
 
 
 let commonModule = angular.module('app.common', [
   Navbar,
-  Hero,
-  User
+  Hero
 ])
 .service('TeamService', TeamService)
 .name;
